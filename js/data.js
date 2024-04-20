02bbee7749a1ad0a0af1f09da66cd5a34bd4609c
WStalker <zlokazov.k@gmail.com>
2024-04-20 11:11:48 +0300

    Выполнены задания "Больше деталей" и "Модуляция"

---
+++ js/data.js
@@ -0,0 +1,77 @@
+export const OFFERS_COUNT = 10;
+
+export const TITLES = [
+  'Апарт-отель М97',
+  'Апартаменты BombFlat На Гривцова',
+  'Апартаменты BombFlat На Можайской',
+  'Отель Best Western Zoom Hotel',
+  'Апартаменты Like apartment SL',
+  'Отель Dell Arte',
+  'Гостиница Barch на Владимирском',
+  'Гостиница Nova'
+];
+
+export const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+export const ROOMS_RANGE = [
+  1,
+  10
+];
+
+export const GUESTS_RANGE = [
+  1,
+  10
+];
+
+export const CHECK_TIME = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+export const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+export const DESCRIPTIONS = [
+  'Апартаменты студия',
+  'Трёхместный Студия в стиле Лофт',
+  'Двухместный номер с 1 кроватью стандарт',
+  'Апартаменты standart',
+  'Двухместный номер с 1 кроватью стандарт с видом на город',
+  'Апартаменты комфортабельные для двоих в самом сердце Петербурга',
+  'Двухместный номер с 2 отдельными кроватями',
+  'Уютная квартира-студия в центре города'
+];
+
+export const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+export const LAT_RANGE = [
+  35.65,
+  35.7
+];
+
+export const LNG_RANGE = [
+  139.7,
+  139.8
+];
+
+export const PRICE_RANGE = [
+  1000,
+  10000
+];
