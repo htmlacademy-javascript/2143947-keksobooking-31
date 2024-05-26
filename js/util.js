de4d9768ae1fff1cd8f2832359237bbab04415cf
WStalker <zlokazov.k@gmail.com>
2024-05-26 21:56:47 +0300

    Из util.js убраны не используемые функции

--- js/util.js
+++ js/util.js
@@ -1,43 +1,5 @@
 const ALERT_SHOW_TIME = 5000;
 
-export const randomNumberArray = (min, max) => {
-  const array = [];
-  for (let i = min; i <= max; i++) {
-    array.push(i);
-  }
-
-  return array;
-};
-
-export const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-
-  return array;
-};
-
-export const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-export const getRandomFloat = (a, b) => {
-  const lower = Math.min(a, b);
-  const upper = Math.max(a, b);
-  const result = Math.random() * (upper - lower) + lower;
-
-  return result;
-};
-
-export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 export const isEscapeKey = (evt) => evt.key === 'Escape';
 
 export const showAlert = (message) => {
